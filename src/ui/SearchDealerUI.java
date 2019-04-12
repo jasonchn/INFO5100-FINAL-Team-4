15efa498e492e08e7fb0d2284917dad3d2385e50
sharma-priyan <sharma.priyan@husky.neu.edu>
2019-04-12 11:25:05 -0700

    Added radio button for search criteria

--- src/ui/SearchDealerUI.java
+++ src/ui/SearchDealerUI.java
@@ -9,30 +9,42 @@ import javax.swing.border.TitledBorder;
 
 public class SearchDealerUI extends JFrame{
 	
-	protected JLabel nameLabel, zipCodeLabel;
+	//protected JLabel nameLabel, zipCodeLabel;
 	protected JTextField nameText, zipCodeText;
 	protected JButton search;
+	protected JRadioButton someRadioButton1;
+	protected JRadioButton someRadioButton2;
+	protected ButtonGroup myButtonGroup;
+	JPanel jpanel;
 	
 	public SearchDealerUI() {
 		createComponents();
 		addComponents();
 		addListeners();
 		makeItVisible();
+		nameText.setEnabled(false);
+		zipCodeText.setEnabled(false);
 	}
 	
 	private void createComponents() {
-		nameLabel = new JLabel("Dealer Name", JLabel.RIGHT);
-		zipCodeLabel = new JLabel("Zip code", JLabel.RIGHT);
+		//nameLabel = new JLabel("Dealer Name", JLabel.RIGHT);
+		//zipCodeLabel = new JLabel("Zip code", JLabel.RIGHT);
 		nameText = new JTextField(10);
+		//nameText.setLocation(10,10);
+		
 		zipCodeText = new JTextField(10);
 		search = new JButton("Search");
+		someRadioButton1= new JRadioButton("Dealers Name");
+		someRadioButton2= new JRadioButton("ZipCode");
+		myButtonGroup = new ButtonGroup();
+		
 	}
 	public void addComponents() {
 		Container con = this.getContentPane();
 		con.setLayout(new BorderLayout());
 
 		// TODO Auto-generated method stub
-		JPanel jpanel = new JPanel();
+		jpanel = new JPanel();
 		jpanel.setLayout(new GridLayout(3, 2));
 		TitledBorder border = new TitledBorder("Dealers in Seattle, WA");
 	    border.setTitleJustification(TitledBorder.CENTER);
@@ -40,12 +52,20 @@ public class SearchDealerUI extends JFrame{
 	 
 	    jpanel.setBorder(border);
 	    
-		jpanel.add(nameLabel);
+		//jpanel.add(nameLabel);
+		
+		//jpanel.add(zipCodeLabel);
+		
+	
+		jpanel.add(someRadioButton1);
 		jpanel.add(nameText);
-		jpanel.add(zipCodeLabel);
+		jpanel.add(someRadioButton2);
 		jpanel.add(zipCodeText);
 		jpanel.add(search);
 		con.add(jpanel, "Center");
+		
+		myButtonGroup.add(someRadioButton1);
+		myButtonGroup.add(someRadioButton2);
 		
 		
 
@@ -58,14 +78,35 @@ public class SearchDealerUI extends JFrame{
 				System.out.println(name +"  "+zipcode);
 			}
 		});
+		someRadioButton1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (someRadioButton1.isSelected()) {
+					nameText.setEnabled(true);
+					zipCodeText.setText("");
+			    }
+			}
+		});
+		someRadioButton2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (someRadioButton2.isSelected()) {
+					zipCodeText.setEnabled(true);
+					nameText.setText("");
+			    }
+			}
+		});
+		
 	}
 	
 	
 	private void makeItVisible() {
-		this.setSize(300, 300);
+		this.setSize(500, 300);
 		this.setVisible(true);
 		//this.pack();
-
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	public static void main(String[] args)  {
